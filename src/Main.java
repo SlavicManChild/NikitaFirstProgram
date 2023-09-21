a5c57d4b30b8ce14637e5ce54523f9517b96543f
BT_1N3_28 <sample@bths.com>
2023-09-21 08:51:54 -0400

    made the adder

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,18 @@ public class Main {
         int peopleQuantity;
         double tipPercent;
         double totalCost;
-        double singleCost;
-        System.out.println("What will the tpip eprcentage be? >");
+        double singleCost = 0;
+
 
         System.out.println("Please enter the cost of each individual item purchased, to end and calculate enter -1");
         double temp = scan.nextDouble();
         singleCost += temp;
-        while (singleCost != -1){
+        while (temp != -1){
             System.out.println("Please enter the cost of each individual item purchased, to end and calculate enter -1");
             temp = scan.nextDouble();
             singleCost += temp;
         }
+        singleCost += 1;
         System.out.print(singleCost);
     }
 }
